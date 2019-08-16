ec966c96d6155be279f55497ce5548beb0e30f85
Caitlyn <cgreffly@gmail.com>
2019-08-16 16:11:46 -0700

    Updates

---
+++ precache-manifest.df6cd9e926d417eb27298b2f0ddb6f9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f33fa60d0c5322111f2d29202b90db7",
+    "url": "/What-To-Wear/index.html"
+  },
+  {
+    "revision": "944bc6a1598216a212c9",
+    "url": "/What-To-Wear/static/css/main.108fe1a6.chunk.css"
+  },
+  {
+    "revision": "552e3f4510a5045c3c7c",
+    "url": "/What-To-Wear/static/js/2.50c0b9c8.chunk.js"
+  },
+  {
+    "revision": "944bc6a1598216a212c9",
+    "url": "/What-To-Wear/static/js/main.db1ea890.chunk.js"
+  },
+  {
+    "revision": "a34f8e06722bfe84fd5e",
+    "url": "/What-To-Wear/static/js/runtime~main.942acf9a.js"
+  }
+]);
